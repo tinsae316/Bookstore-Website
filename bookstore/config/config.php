4659a07e98f94424736932b5e9c10e104d67eefc
tinsae316 <123117343+tinsae316@users.noreply.github.com>
2024-05-25 09:58:48 -0700

    first commit

---
+++ bookstore/config/config.php
@@ -0,0 +1,25 @@
+<?php 
+
+//host
+$host = "localhost";
+
+//dbname
+$dbname = "bookstore";
+
+//username
+$user = "root";
+
+//password
+
+$pass = "";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+//if($conn) {
+  //  echo "Connected successfully";
+//}
+//else {
+//    echo "Connection failed";
+//}
