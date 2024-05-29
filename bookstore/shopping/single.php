c33d730cc670cb28441a61f792b1ce6f50696ef0
tinsae316 <123117343+tinsae316@users.noreply.github.com>
2024-05-29 04:52:36 -0700

    cart

--- bookstore/shopping/single.php
+++ bookstore/shopping/single.php
@@ -3,9 +3,37 @@
 
 <?php 
 
+    if(isset($_POST['submit'])){
+        $pro_id = $_POST['pro_id'];
+        $pro_name = $_POST['pro_name'];
+        $pro_image = $_POST['pro_image'];
+        $pro_price = $_POST['pro_price'];
+        $pro_amount = $_POST['pro_amount'];
+        $pro_file = $_POST['pro_file'];
+        $user_id = $_POST['user_id'];
+
+        $insert = $conn->prepare("INSERT INTO cart (pro_id, pro_name, pro_image, pro_price, pro_amount, pro_file, user_id) VALUES (:pro_id, :pro_name, :pro_image, :pro_price, :pro_amount, :pro_file, :user_id)");
+
+        $insert->execute([
+            ':pro_id' => $pro_id,
+            ':pro_name' => $pro_name,
+            ':pro_image' => $pro_image,
+            ':pro_price' => $pro_price,
+            ':pro_amount' => $pro_amount,
+            ':pro_file' => $pro_file,
+            ':user_id' => $user_id,
+        ]);
+    }
+
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-
+        //checking for product in cart
+        if(isset($_SESSION['user_id'])){
+            $select = $conn->query("SELECT * FROM cart WHERE pro_id = '$id' AND user_id='$_SESSION[user_id]'");
+            $select->execute();
+        }
+        
+        //getting data for every product
         $row = $conn->query("SELECT * FROM product WHERE status = 1 AND id='$id'");
         $row->execute();
         $product = $row->fetch(PDO::FETCH_OBJ);
@@ -16,7 +44,7 @@
     }
 
 ?>
-        <div class="row d-flex justify-content-center">
+        <div class="row d-flex justify-content-center mt-4">
             <div class="col-md-10">
                 <div class="card">
                     <div class="row">
@@ -36,12 +64,72 @@
                                     </div>
                                 </div>
                                 <p class="about"><?php echo $product->description; ?></p>
-                              
-                                <div class="cart mt-4 align-items-center"> <button class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button> </div>
+                                <form method="post" id="form-data">
+                                    <div class="">
+                                        <input type="hidden" name="pro_id" value="<?php echo $product->id; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="hidden" name="pro_name" value="<?php echo $product->name; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="hidden" name="pro_image" value="<?php echo $product->image; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="hidden" name="pro_price" value="<?php echo $product->price; ?>" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="hidden" name="pro_amount" value="1" class="form-control">
+                                    </div>
+                                    <div class="">
+                                        <input type="hidden" name="pro_file" value="<?php echo $product->file; ?>" class="form-control">
+                                    </div>
+                                    <?php if(isset($_SESSION['user_id'])) : ?>
+                                        <div class="">
+                                        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" class="form-control">
+                                        </div>
+                                    <?php endif; ?>
+                                    
+                                    
+                                        
+                                    <div class="cart mt-4 align-items-center">
+                                        <?php if(isset($_SESSION['user_id'])) : ?>
+                                            <?php if($select->rowCount() > 0) : ?>
+                                                <button id="submit" name="submit" type="submit" disabled class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Added to cart</button>
+                                            <?php else : ?>
+                                                <button id="submit" name="submit" type="submit" class="btn btn-primary text-uppercase mr-2 px-4"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+                                            <?php endif; ?>
+                                        <?php endif; ?>
+
+                                        
+                                            
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <?php require "../includes/footer.php"; ?>
+<?php require "../includes/footer.php"; ?>
+
+<script>
+    $(document).ready(function(){
+        
+        $(document).on("submit", function(e){
+
+            e.preventDefault();
+            var formdata = $("#form-data").serialize()+'&submit=submit';
+
+            $.ajax({
+                type: "post",
+                url: "single.php?id=<?php echo $id; ?>",
+                data: formdata,
+
+                success: function() {
+                    alert("Product added to cart successfully");
+                    $("#submit").html("<i class='fas fa-shopping-cart'></i> Added to cart").prop("disabled", true);
+                }
+            })
+        })
+    });
+</script>
